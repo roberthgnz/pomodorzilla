61ac5ee167432805fa2ce48d2b26368461fd88fe
R. Eliut <63687573+eliutdev@users.noreply.github.com>
2022-02-25 00:39:27 +0100

    dfdf

--- src/App.jsx
+++ src/App.jsx
@@ -29,72 +29,87 @@ function App() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h1>Pomodorzilla</h1>
-      <h2>Timer settings</h2>
-      <div>
-        <span>Time (minutes):</span>
-      </div>
-      <div className="control-horizontal">
+    <>
+      <header
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <h1>Pomodorzilla</h1>
+        <a href="https://github.com/eliutdev/pomodorzilla" target="_blank">
+          GitHub
+        </a>
+      </header>
+      <form onSubmit={handleSubmit}>
+        <h2>Timer settings</h2>
         <div>
-          <label htmlFor="pomodoro">Pomodoro</label>
-          <input
-            type="number"
-            id="pomodoro"
-            value={config.pomodoro}
-            onChange={(e) =>
-              setConfig({ ...config, pomodoro: parseInt(e.target.value) })
-            }
-          />
+          <span>Time (minutes):</span>
+        </div>
+        <div className="control-horizontal">
+          <div>
+            <label htmlFor="pomodoro">Pomodoro</label>
+            <input
+              type="number"
+              id="pomodoro"
+              value={config.pomodoro}
+              onChange={(e) =>
+                setConfig({ ...config, pomodoro: parseInt(e.target.value) })
+              }
+            />
+          </div>
+          <div>
+            <label htmlFor="short-break">Short Break</label>
+            <input
+              type="number"
+              id="short-break"
+              value={config.shortBreak}
+              onChange={(e) =>
+                setConfig({ ...config, shortBreak: parseInt(e.target.value) })
+              }
+            />
+          </div>
+          <div>
+            <label htmlFor="long-break">Long Break</label>
+            <input
+              type="number"
+              id="long-break"
+              value={config.longBreak}
+              onChange={(e) =>
+                setConfig({ ...config, longBreak: parseInt(e.target.value) })
+              }
+            />
+          </div>
         </div>
         <div>
-          <label htmlFor="short-break">Short Break</label>
+          <label htmlFor="long-break-interval">Long Break Interval</label>
           <input
             type="number"
-            id="short-break"
-            value={config.shortBreak}
+            id="long-break-interval"
+            value={config.longBreakInterval}
             onChange={(e) =>
-              setConfig({ ...config, shortBreak: parseInt(e.target.value) })
+              setConfig({
+                ...config,
+                longBreakInterval: parseInt(e.target.value),
+              })
             }
           />
+          <small>(number of pomodoros before a long break)</small>
         </div>
-        <div>
-          <label htmlFor="long-break">Long Break</label>
-          <input
-            type="number"
-            id="long-break"
-            value={config.longBreak}
-            onChange={(e) =>
-              setConfig({ ...config, longBreak: parseInt(e.target.value) })
-            }
-          />
-        </div>
-      </div>
-      <div>
-        <label htmlFor="long-break-interval">Long Break Interval</label>
-        <input
-          type="number"
-          id="long-break-interval"
-          value={config.longBreakInterval}
-          onChange={(e) =>
-            setConfig({
-              ...config,
-              longBreakInterval: parseInt(e.target.value),
-            })
-          }
-        />
-        <small>(number of pomodoros before a long break)</small>
-      </div>
-      <button type="submit" disabled={loading}>
-        {loading ? "Generating..." : "Generate"}
-      </button>
-      {url && (
-        <p className="url">
-          <small>Copy and paste this URL into your OBS to start working</small>
-          <a href={url}>{url}</a>
-        </p>
-      )}
-    </form>
+        <button type="submit" disabled={loading}>
+          {loading ? "Generating..." : "Generate url"}
+        </button>
+        {url && (
+          <p className="url">
+            <small>
+              Copy and paste this URL into your OBS to start working
+            </small>
+            <a href={url}>{url}</a>
+          </p>
+        )}
+      </form>
+    </>
   );
 }
 
