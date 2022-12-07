c7e1ae43833cf7db1163626d2d3a143fd9c7b8df
rxb3rth <reliutg@gmail.com>
2022-12-07 21:07:33 +0100

    fix: react app root render

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+import { createRoot } from 'react-dom/client';
+import App from './App';
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
